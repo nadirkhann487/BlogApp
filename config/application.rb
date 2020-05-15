b4e71a1783aecc190814850f41f2addcbab2ae03
Nadir Khan <nadirkhann487@gmail.com>
2020-05-15 07:22:25 +0100

    Precompile assets1

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
-require 'rails/all'
-
+require 'sprockets/railtie'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,7 +9,6 @@ module AdminExample
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-	config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
